10ad4680f66b90fac964e5e025905cd6e84d32a1
alstjr5949 <alstjr5949@gmail.com>
2022-09-27 23:39:44 +0900

    refactor : 한국어로 데이터를 받아오기 위한 url 변경

--- src/api.ts
+++ src/api.ts
@@ -40,61 +40,61 @@ export interface IGetTvsResult {
 }
 
 export function getMovies() {
-  return fetch(`${BASE_PATH}/movie/now_playing?api_key=${API_KEY}`).then(
-    (response) => response.json()
-  );
+  return fetch(
+    `${BASE_PATH}/movie/now_playing?api_key=${API_KEY}&language=ko-KR&region=kr`
+  ).then((response) => response.json());
 }
 
 export function getPopularMovies() {
-  return fetch(`${BASE_PATH}/movie/popular?api_key=${API_KEY}`).then(
-    (response) => response.json()
-  );
+  return fetch(
+    `${BASE_PATH}/movie/popular?api_key=${API_KEY}&language=ko-KR&region=kr`
+  ).then((response) => response.json());
 }
 
 export function getTopMovies() {
-  return fetch(`${BASE_PATH}/movie/top_rated?api_key=${API_KEY}`).then(
-    (response) => response.json()
-  );
+  return fetch(
+    `${BASE_PATH}/movie/top_rated?api_key=${API_KEY}&language=ko-KR&region=kr`
+  ).then((response) => response.json());
 }
 
 export function getUpMovies() {
-  return fetch(`${BASE_PATH}/movie/upcoming?api_key=${API_KEY}`).then(
-    (response) => response.json()
-  );
+  return fetch(
+    `${BASE_PATH}/movie/upcoming?api_key=${API_KEY}&language=ko-KR&region=kr`
+  ).then((response) => response.json());
 }
 
 export function getAiringTv() {
-  return fetch(`${BASE_PATH}/tv/airing_today?api_key=${API_KEY}`).then(
-    (response) => response.json()
-  );
+  return fetch(
+    `${BASE_PATH}/tv/airing_today?api_key=${API_KEY}&language=ko-KR&region=kr`
+  ).then((response) => response.json());
 }
 
 export function getOnAirTv() {
-  return fetch(`${BASE_PATH}/tv/on_the_air?api_key=${API_KEY}`).then(
-    (response) => response.json()
-  );
+  return fetch(
+    `${BASE_PATH}/tv/on_the_air?api_key=${API_KEY}&language=ko-KR&region=kr`
+  ).then((response) => response.json());
 }
 
 export function getPopularTv() {
-  return fetch(`${BASE_PATH}/tv/popular?api_key=${API_KEY}`).then((response) =>
-    response.json()
-  );
+  return fetch(
+    `${BASE_PATH}/tv/popular?api_key=${API_KEY}&language=ko-KR&region=kr`
+  ).then((response) => response.json());
 }
 
 export function getTopTv() {
-  return fetch(`${BASE_PATH}/tv/top_rated?api_key=${API_KEY}`).then(
-    (response) => response.json()
-  );
+  return fetch(
+    `${BASE_PATH}/tv/top_rated?api_key=${API_KEY}&language=ko-KR&region=kr`
+  ).then((response) => response.json());
 }
 
 export function getSearchMovieData(queryData: string) {
   return fetch(
-    `${BASE_PATH}/search/movie?api_key=${API_KEY}&query=${queryData}`
+    `${BASE_PATH}/search/movie?api_key=${API_KEY}&query=${queryData}&language=ko-KR&region=kr`
   ).then((response) => response.json());
 }
 
 export function getSearchTvData(queryData: string) {
   return fetch(
-    `${BASE_PATH}/search/tv?api_key=${API_KEY}&query=${queryData}`
+    `${BASE_PATH}/search/tv?api_key=${API_KEY}&query=${queryData}&language=ko-KR&region=kr`
   ).then((response) => response.json());
 }
